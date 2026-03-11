06ab8e2dec048f4410bba85632748e9cbf0aa120
Snowialex <alexinge13@gmail.com>
2026-03-10 18:41:35 -0600

    El logo en header no se amontona

--- front-end/aqua-react/src/components/Header.jsx
+++ front-end/aqua-react/src/components/Header.jsx
@@ -75,8 +75,8 @@ function Header({ abrirCarrito, abrirLogin, usuario, cerrarSesion, conteo }) {
       <div className="header-content">
         <div className="logo">
           <img src="/imagenes/logo1.1.png" className="logo-img" alt="Logo" />
-          <span>
-            Macro <span className="pro">Water Solutions</span>
+          <span className="logo-text">
+            Macro <span className="pro logo-ws">Water Solutions</span>
           </span>
         </div>
 
